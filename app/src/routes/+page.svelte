9d16bc0e8d2669a51867e3c113db7f8537d6df0b
Jan Serbousek <serbousek.jan@seznam.cz>
2024-04-22 09:25:51 +0200

    Pole pro piškvorky

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Piškvorky</title>
+	<meta name="description" content="Piškvorky demo app" />
 </svelte:head>
 
 <section>
 	<h1>Piškvorky</h1>
+	<div id="board">
+		{#each Array(3).fill() as _, row}
+			<div class="row">
+				{#each Array(3).fill() as _, col}
+					<div class="cell"></div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </section>
-
-
